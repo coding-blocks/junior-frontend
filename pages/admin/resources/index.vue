d409c6b8cea3056c8b0c26806918555a609c17b8
Pulkit Aggarwal <aggarwalpulkit596@gmail.com>
2020-11-08 14:51:37 +0530

    add resources

--- pages/admin/resources/index.vue
+++ pages/admin/resources/index.vue
@@ -1,11 +1,41 @@
 <template>
-  <h1>
-    Resources Page
-  </h1>
+  <div>
+    <div class="search-bar">
+      <input type="text" placeholder="Search Here" />
+      <i class="fas fa-search icon"></i>
+    </div>
+    <ul class="list-divided mt-4">
+      <li class="resources flex items-center px-4 py-3" v-for="resource in resources" :key="resource.id">
+        <div>{{ resource.title }} </div>
+        <div> 
+          <nuxt-link class="button-solid button-orange mx-auto" :to="`/admin/resources/${resource.id}`"> Edit</nuxt-link>
+        </div>
+      </li>
+    </ul>
+    <nuxt-link class="button-solid button-orange mx-auto mt-4" to="/admin/resources/add"> Create Resource </nuxt-link>
+  </div>
 </template>
+
+
 <script lang="ts">
-import Vue from 'vue'
+import Vue from 'vue';
+import { mapGetters, mapActions } from 'vuex';
+import Button from '@/components/Base/Button.vue';
+
 export default Vue.extend({
-  
+  components: {},
+  mounted() {
+    this.fetchResources()
+  },
+  computed: {
+    ...mapGetters({
+      resources: 'resources/list'
+    })
+  },
+  methods: {
+    ...mapActions({
+      fetchResources: 'resources/fetchAll'
+    })
+  }
 })
 </script>
