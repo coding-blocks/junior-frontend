7c2c096566f5fac211f3a77dc58f762e29fca4a7
Jatin Katyal <jatin.katyal13@gmail.com>
2020-11-09 22:52:32 +0530

    remove: iFrame from vue form generator

--- plugins/vue-form-generator/index.ts
+++ plugins/vue-form-generator/index.ts
@@ -1,9 +1,5 @@
 import Vue from 'vue'
 import VueFormGenerator from 'vue-form-generator'
 import 'vue-form-generator/dist/vfg.css'
-// import Iframe from "./Iframe.vue";
-
-//  Vue.component("Iframe", Iframe);
-
 
 Vue.use(VueFormGenerator)
