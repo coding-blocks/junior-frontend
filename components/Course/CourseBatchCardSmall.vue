3983895820cae7abdb17e79a9691c016d5549723
BipinKalra <bipinkalra@gmail.com>
2020-11-18 18:46:21 +0530

    new card

---
+++ components/Course/CourseBatchCardSmall.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="card">
+    <div class="row no-gutters justify-content-between align-items-center">
+      <div class="br-20 bg-white all-center p-10">
+        <img src="https://cb-thumbnails.s3.ap-south-1.amazonaws.com/python-purple.svg">
+      </div>
+      <div class="flex-1 t-align-r premium-tag side-tag side-tag--right">
+
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+  .premium-card {
+    background: linear-gradient(90deg, #4f33da, #6536de);
+  }
+
+  .premium-tag {
+    display: none;
+  }
+
+  .premium-card > .premium-tag {
+    display: block;
+  }
+</style>
