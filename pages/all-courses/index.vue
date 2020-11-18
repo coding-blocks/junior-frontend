02b7148e2e75ba2c17c0a9dd766498ddf95932d4
BipinKalra <bipinkalra@gmail.com>
2020-11-18 14:53:33 +0530

    pills

---
+++ pages/all-courses/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="py-50 px-xl-100 px-lg-75 px-md-50 px-sm-30 px-20">
+    <div class="container">
+      <div class="row no-gutters align-items-center justify-content-between">
+        <div class="flex-1">
+          <div class="heading-5 bold mb-15">
+            Let us help to find you the perfect course!
+          </div>
+          <div class="heading-6 grey-light-1">
+            Which class are you/your kid in?
+          </div>
+        </div>
+        <div class="col-6">
+          <div
+            class="row no-gutters justify-content-between align-items-center"
+          >
+            <div class="pill-tag pill-tag--hoverable heading-6 bold mr-30">
+              1-3
+            </div>
+            <div class="pill-tag pill-tag--hoverable heading-6 bold mr-30">
+              1-3
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script></script>
