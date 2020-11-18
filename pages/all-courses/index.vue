0d96f81285c87430302496c896b0f200bb015521
BipinKalra <bipinkalra@gmail.com>
2020-11-18 14:58:16 +0530

    pill fixes

--- pages/all-courses/index.vue
+++ pages/all-courses/index.vue
@@ -2,23 +2,31 @@
   <div class="py-50 px-xl-100 px-lg-75 px-md-50 px-sm-30 px-20">
     <div class="container">
       <div class="row no-gutters align-items-center justify-content-between">
-        <div class="flex-1">
+        <div class="flex-1 pr-25">
           <div class="heading-5 bold mb-15">
             Let us help to find you the perfect course!
           </div>
-          <div class="heading-6 grey-light-1">
+          <div class="heading-6 text-grey-light-1">
             Which class are you/your kid in?
           </div>
         </div>
-        <div class="col-6">
+        <div class="col-md-6">
           <div
             class="row no-gutters justify-content-between align-items-center"
           >
-            <div class="pill-tag pill-tag--hoverable heading-6 bold mr-30">
+            <div class="pill-tag pill-tag--hoverable heading-6 bold">
               1-3
             </div>
-            <div class="pill-tag pill-tag--hoverable heading-6 bold mr-30">
-              1-3
+            <div
+              class="pill-tag pill-tag--hoverable heading-6 bold active"
+            >
+              4-6
+            </div>
+            <div class="pill-tag pill-tag--hoverable heading-6 bold">
+              7-9
+            </div>
+            <div class="pill-tag pill-tag--hoverable heading-6 bold">
+              10-12
             </div>
           </div>
         </div>
