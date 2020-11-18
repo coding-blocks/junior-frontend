defdc0081f219d1c0afd9a9ba8613290b1d10a36
BipinKalra <bipinkalra@gmail.com>
2020-11-18 16:36:24 +0530

    details

---
+++ pages/all-courses/course-details/index.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="pt-50">
+    <div
+      class="p-xl-100 p-lg-75 p-md-50 p-sm-30 p-20 br-20 position-relative white bg-gradient-purple"
+    >
+      <div class="container">
+        <div class="row align-items-center">
+          <div class="col-lg-7">
+            <div class="row no-gutters align-items-center">
+              <img
+                src="https://cb-thumbnails.s3.ap-south-1.amazonaws.com/python-white-big.svg"
+              />
+              <div class="flex-1 pl-30">
+                <div class="font-5">RECOMMENDED FOR 7-9</div>
+                <div class="mt-5 white heading-2 bold">
+                  Introduction to Python
+                </div>
+              </div>
+            </div>
+            <div class="font-5 mt-40">
+              Python is a great programming language with which to start
+              learning to code. Short, colorful apps and games you &amp; your
+              child can program right away!
+            </div>
+            <div class="heading-6 bold mt-40">What will you learn?</div>
+            <div class="mt-40 row no-gutters align-items-center">
+              <div class="pill-tag mr-20 mb-20">Introduction to Python</div>
+              <div class="pill-tag mr-20 mb-20">Animations</div>
+              <div class="pill-tag mr-20 mb-20">Desktop Games</div>
+              <div class="pill-tag mr-20 mb-20">Introduction to AI</div>
+              <div class="pill-tag mr-20 mb-20">Falana Dhimaka</div>
+            </div>
+          </div>
+          <div class="col-lg-5 mt-lg-none mt-50">
+            <div
+              class="br-20 position-relative mb-20 mx-auto"
+              style="max-width: 450px"
+            >
+              <img
+                src="https://cb-thumbnails.s3.ap-south-1.amazonaws.com/varun-bhaiya-python.svg"
+                class="br-20 w-100"
+              />
+              <button
+                class="button-primary w-75 position-absolute justify-content-center"
+                style="left: 12.5%; bottom: -20px"
+              >
+                Start Learning
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
