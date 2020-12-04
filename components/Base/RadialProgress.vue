4a79355acd20a058934f0274720d7dc283ed6194
BipinKalra <bipinkalra@gmail.com>
2020-12-04 14:34:01 +0530

    radial progress element done

---
+++ components/Base/RadialProgress.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="s-50x50">
+    <svg
+      class="radial-progress orange"
+      :data-progress="progress"
+      x="0px"
+      y="0px"
+      viewBox="0 0 80 80"
+    >
+      <path class="track" d="M5,40a35,35 0 1,0 70,0a35,35 0 1,0 -70,0" />
+      <path class="fill" d="M5,40a35,35 0 1,0 70,0a35,35 0 1,0 -70,0" />
+    </svg>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['progress'],
+}
+</script>
