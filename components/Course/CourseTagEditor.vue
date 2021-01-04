a80327efa5b7c164ae69ec15c577498981f94977
Jatin Katyal <jatin.katyal13@gmail.com>
2021-01-04 23:50:45 +0530

    fix: course tag editor

--- components/Course/CourseTagEditor.vue
+++ components/Course/CourseTagEditor.vue
@@ -13,30 +13,30 @@
         loadingText="Adding"
       />
     </div>
-          <VAsync :task="fetchTagsTask">
-          <template v-slot="{ value: tags }">
-    <ul class="list-divided mt-4">
-      <li
-        class="tags flex-row justify-content-around items-center px-4 py-3"
-        v-for="tag in tags"
-        :key="tag.id"
-      >
-        <div class="mt-20 font-5 t-align-c">
-          {{ tag.title }}
-        </div>
-        <div>
-          <TaskButton
-            class="h-50"
-            :task="removeTagTask"
-            :params="[tag]"
-            text="Remove"
-            loadingText="Removing"
-          />
-        </div>
-      </li>
-    </ul>
-          </template>
-          </VAsync>
+    <VAsync :task="fetchTagsTask">
+      <template v-slot="{ value: tags }">
+        <ul class="list-divided mt-4">
+          <li
+            class="tags flex-row justify-content-around items-center px-4 py-3"
+            v-for="tag in tags"
+            :key="tag.id"
+          >
+            <div class="mt-20 font-5 t-align-c">
+              {{ tag.title }}
+            </div>
+            <div>
+              <TaskButton
+                class="h-50"
+                :task="removeTagTask"
+                :params="[tag]"
+                text="Remove"
+                loadingText="Removing"
+              />
+            </div>
+          </li>
+        </ul>
+      </template>
+    </VAsync>
   </div>
 </template>
 
@@ -78,11 +78,42 @@ export default Vue.extend({
   tasks(t) {
     return {
       addTagTask: t(async function () {
-        return TagRepository.create(this.tag);
-                this.fetchTagsTask.run()
+        try {
+          if (!this.tag.title) {
+            this.$notify({
+              group: 'admin',
+              title: 'Tag cant be empty'
+            })
+            return;
+          }
+          await TagRepository.create(this.tag);
+          this.fetchTagsTask.run();
+          this.$notify({
+            group: 'admin',
+            title: 'Added Successfully'
+          })
+        } catch (err) {
+          this.$notify({
+            group: 'admin',
+            title: 'Error !'
+          })
+        }
       }),
-      removeTagTask: t(async function (feature) {
-        return this.features.splice(this.features.indexOf(feature), 1);
+      removeTagTask: t(async function (tag) {
+        try {
+          await TagRepository.delete(tag.id)
+          this.fetchTagsTask.run();
+          this.$notify({
+            group: 'admin',
+            title: 'Removed Successfully'
+          })
+        } catch (err) {
+          console.log(err)
+          this.$notify({
+            group: 'admin',
+            title: 'Error !'
+          })
+        }
       }),
       fetchTagsTask: t(function *() {
         return TagRepository.fetchAll();
